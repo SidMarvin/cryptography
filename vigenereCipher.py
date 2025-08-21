62665d2b781a046d6b4c98d73be79cb948378665
SidMarvin <66871853+SidMarvin@users.noreply.github.com>
2025-08-21 18:02:34 +0300

    Update vigenereCipher.py

--- vigenereCipher.py
+++ vigenereCipher.py
@@ -1,3 +1,9 @@
+"""
+необходимо добавить условие, когда переменная ciphersymbol_number (это индекс буквы шифртекста) превышает длину алфавита, ибо мы получаем ошибку IndexError: string index out of range 
+"""
+
+
+
 alphavite_rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 alphavite_eng = "abcdefghijklmnopqrstuvwxyz"
 
@@ -8,15 +14,68 @@ if mod == "1" or mod == "e":
     if text_lang == "1":
         print("Выбран русский язык.")
         open_text = input("Введите текст для зашифровки: ")
-        input_key = input("Введите ключ")
+        input_key = input("Введите ключ: ")
+        close_text = ""
         #проверка на символы (должны быть только буквы)
         #проверка что ключ не длиннее текста
-        lenght_key = len(input_key)
+        lenght_key = len(input_key) #длина ключа
         
-        for i in open_text.lower():
-            if i in alphavite_rus:
-                number_open_text = alphavite_rus.index(i) + key #индекс буквы открытого текста
-                #идея следующая: перебирать индекс букв ключа от нуля до конца его длины в цикле while
+        len_ot = len(open_text)
+        len_it = len(input_key)
+        
+        #если длина текста больше длины ключа
+        if len_ot > len_it: 
+            #если длина текста кратна длине ключа
+            if len_ot % len_it == 0:
+                delta = len(open_text) // len(input_key)
+                input_key = input_key * delta
+
+                #шифрование
+                for i, j in zip(open_text.lower(), input_key.lower()):
+                    if i in alphavite_rus:
+                        number_open_text = alphavite_rus.index(i) #индекс буквы открытого текста
+                        number_key = alphavite_rus.index(j) #индекс буквы ibahntrcnf текста
+                        ciphersymbol_number = number_open_text + number_key #индекс зашифрованной буквы
+                        close_text += (alphavite_rus[ciphersymbol_number]) 
+                    else:
+                        pass
+                        
+            #если длина текста НЕ кратна длине ключа            
+            elif len_ot % len_it != 0:
+                delta = len(open_text) // len(input_key)
+                input_key = input_key * delta + input_key[:(len(open_text) % len(input_key))]
+                #шифрование
+                for i, j in zip(open_text.lower(), input_key.lower()):
+                    if i in alphavite_rus:
+                        number_open_text = alphavite_rus.index(i) #индекс буквы открытого текста
+                        number_key = alphavite_rus.index(j) #индекс буквы ключа
+                        ciphersymbol_number = number_open_text + number_key #индекс зашифрованной буквы
+                        close_text += (alphavite_rus[ciphersymbol_number])
+
+        #если длина текста меньше длины ключа
+        elif len_ot < len_it: 
+            input_key = input_key[:len(open_text)]
+            #шифрование
+            for i, j in zip(open_text.lower(), input_key.lower()):
+                if i in alphavite_rus:
+                    number_open_text = alphavite_rus.index(i) #индекс буквы открытого текста
+                    number_key = alphavite_rus.index(j) #индекс буквы ibahntrcnf текста
+                    ciphersymbol_number = number_open_text + number_key #индекс зашифрованной буквы
+                    close_text += (alphavite_rus[ciphersymbol_number]) 
+            
+        #если длина текста равна длине ключа
+        elif len_ot == len_it: 
+            #шифрование
+            for i, j in zip(open_text.lower(), input_key.lower()):
+                if i in alphavite_rus:
+                    number_open_text = alphavite_rus.index(i) #индекс буквы открытого текста
+                    number_key = alphavite_rus.index(j) #индекс буквы ibahntrcnf текста
+                    ciphersymbol_number = number_open_text + number_key #индекс зашифрованной буквы
+                    close_text += (alphavite_rus[ciphersymbol_number]) 
+            
+        print(close_text)       
+
+                       
     
 elif mod == "2" or mod == "d":
     text_lang = input("Выберите язык для расшифрования: \n[1] Русский \n[2] Английский\n: ")
